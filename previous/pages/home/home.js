960fbbd90ad85eb3a87729e7ceb5d4906041d8b1
luvolunov <luvolunov@gmail.com>
2019-10-27 14:37:40 +0900

    also fixes

--- previous/pages/home/home.js
+++ previous/pages/home/home.js
@@ -1,5 +1,6 @@
 import "./home.scss"
 import "../root"
+import "@components/clipboard-input"
 
 const tabs = document.getElementsByClassName("tabs__tab"),
       buttons = document.getElementsByClassName("control-button__input");
@@ -15,5 +16,19 @@ for (let i = 0; i < tabs.length; ++i){
   buttons[i].addEventListener("change", function(){
     document.getElementsByClassName("activity")[0].className = "tabs__tab";
     tabs[i].className = "tabs__tab activity";
+  })
+}
+
+
+const smallTabs = document.getElementsByClassName("manuals__tab"),
+      input = document.getElementsByClassName("manuals__input");
+
+input[0].checked = true;
+
+for (let i = 0; i < smallTabs.length; ++i){
+  smallTabs[i].addEventListener("click", function(){
+    input[i].checked = true;
+    document.getElementsByClassName("active")[0].className = "manuals__tab";
+    this.className = "manuals__tab active"
   })
 }
